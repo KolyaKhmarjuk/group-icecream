9fcbfa8f10bb55cad0966db695e3af39fccf8bf3
KolyaKhmarjuk <106625975+KolyaKhmarjuk@users.noreply.github.com>
2022-09-10 20:13:11 +0000

    Deploying to gh-pages from @ KolyaKhmarjuk/group-icecream@b5d52a2f328fcf4699b990a7d423f0e54d878c97 🚀

--- index.3dda1333.js
+++
@@ -1,2 +0,0 @@
-var forEach=function(e,t,r){if("[object Object]"===Object.prototype.toString.call(e))for(var c in e)Object.prototype.hasOwnProperty.call(e,c)&&t.call(r,e[c],c,e);else for(var a=0,l=e.length;l>a;a++)t.call(r,e[a],a,e)},hamburgers=document.querySelectorAll(".hamburger");hamburgers.length>0&&forEach(hamburgers,(function(e){e.addEventListener("click",(function(){this.classList.toggle("is-active")}),!1)}));
-//# sourceMappingURL=index.3dda1333.js.map
